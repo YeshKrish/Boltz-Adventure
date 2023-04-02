e2233854cb6a8e66920e16592847c6fb8e0562b4
YeshKrish <yeshwinkrishna@gmail.com>
2023-04-02 19:07:42 +0530

    Customize Player Logic start
    Former-commit-id: 66547bd8e1625c921f23becc169892c0f040bcd3

---
+++ Assets/Scripts/Managers/CustomizeManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizeManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    private GameObject _defaultBall;
+    [SerializeField]
+    private ChooseBall _ballPool;
+
+    public static CustomizeManager Instance;
+
+    
+    
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+
+        _defaultBall = _ballPool.BallPool[0];
+        _defaultBall.SetActive(true);
+        DontDestroyOnLoad(Instance);
+    }
+
+    public void ActivateParticularBall(int ballId)
+    {
+        for (int i = 0; i < _ballPool.BallPool.Length; i++)
+        {
+            if(i == ballId)
+            {
+                _ballPool.BallPool[i].SetActive(true);
+                _defaultBall.SetActive(false);
+            }
+        }
+    }
+
+}

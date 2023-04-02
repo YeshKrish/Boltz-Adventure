66547bd8e1625c921f23becc169892c0f040bcd3
YeshKrish <yeshwinkrishna@gmail.com>
2023-04-02 19:07:42 +0530

    Customize Player Logic start

---
+++ Assets/Scripts/ChooseBall.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "BallPool")]
+public class ChooseBall : ScriptableObject
+{
+    public GameObject[] BallPool;
+}

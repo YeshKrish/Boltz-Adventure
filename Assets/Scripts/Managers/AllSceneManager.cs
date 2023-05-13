d0bce14d8c2f96cf3d191c4871558b2d947bfa06
YeshKrish <yeshwinkrishna@gmail.com>
2023-05-14 00:24:36 +0530

    Glass Shader
    Former-commit-id: 3ebc4169a4393c42699bbf95b5620789f4f5ad07

--- Assets/Scripts/Managers/AllSceneManager.cs
+++ Assets/Scripts/Managers/AllSceneManager.cs
@@ -15,6 +15,10 @@ public class AllSceneManager : MonoBehaviour
         {
             instance = this;
         }
+        else
+        {
+            Destroy(this);
+        }
 
         DontDestroyOnLoad(this);
 

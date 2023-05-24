4d1c208106e1a06f94682162172431cbae6732fa
YeshKrish <yeshwinkrishna@gmail.com>
2023-05-25 01:14:26 +0530

    Owl Disappeared SO logic and level select logics

---
+++ Assets/Scripts/LevelSelectScriptableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LevelSelectScreen")]
+public class LevelSelectScriptableObject : ScriptableObject
+{
+    public bool IsOwlDisappereadOnce = false;
+}
